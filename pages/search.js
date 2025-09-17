2e28300b35f16528428b66cb2d53a5519680f3c5
Adrian Markowski <markowski.adrian525@gmail.com>
2025-09-17 09:54:33 +0200

    Rename header.js to Header.js

--- pages/search.js
+++ pages/search.js
@@ -1,8 +1,8 @@
 import { useRouter } from "next/router";
 import { useEffect, useState } from "react";
 import Link from "next/link";
-import Header from "../components/header";
-import Footer from "../components/footer";
+import Header from "../components/Header";
+import Footer from "../components/Footer";
 import { getNextStaticProps } from "@faustwp/core";
 
 export default function SearchResults() {
